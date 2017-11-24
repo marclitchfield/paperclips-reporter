cd98bc3ab4f17f912b24c83fd3b1bae6c2d1632a
Marc Litchfield <marclitchfield@gmail.com>
2017-11-23 22:10:16 -0800

    Initial commit

---
+++ paperclips-reporter.js
@@ -0,0 +1,108 @@
+(() => {
+  if (window.__paperclips_reporter === true)
+    return;
+  window.__paperclips_reporter = true;
+
+  const GAME_KEY = 'reporter-game-key';
+  const TIME_OFFSET = 'reporter-time-offset';
+
+  const firebaseScript = document.createElement('script');
+  firebaseScript.src = 'https://www.gstatic.com/firebasejs/4.6.2/firebase.js';
+  firebaseScript.onload = onScriptLoaded;
+  document.getElementsByTagName("head")[0].appendChild(firebaseScript);
+
+  function onScriptLoaded() {
+    // ?apiKey=___&authDomain=___&databaseURL=___&projectId=___&storageBucket=___&messagingSenderId=___
+    const urlParams = new URLSearchParams(window.location.search);
+    const config = {
+      apiKey: urlParams.get('apiKey'),
+      authDomain: urlParams.get('authDomain'),
+      databaseURL: urlParams.get('databaseURL'),
+      projectId: urlParams.get('projectId'),
+      storageBucket: urlParams.get('storageBucket'),
+      messagingSenderId: urlParams.get('messagingSenderId')
+    };
+    firebase.initializeApp(config);
+
+    const game = getGame(firebase);
+    const metrics = game.child('metrics');
+
+    const sessionTimeOffset = parseInt(window.localStorage.getItem(TIME_OFFSET)) || 0;
+    const sessionStartTimestamp = Date.now();
+
+    setInterval(collectMetrics(metrics, sessionTimeOffset, sessionStartTimestamp), 5000);
+  }
+
+  function getGame(firebase) {
+    const gameKey = window.localStorage.getItem(GAME_KEY);
+    if (!gameKey) {
+      const games = firebase.database().ref('games');
+      const game = games.push();
+      window.localStorage.setItem(GAME_KEY, game.key);
+      return game;
+    }
+    else {
+      return firebase.database().ref('games/' + gameKey);
+    }
+  }
+
+  function exists(id) {
+    return el(id) !== null && el(id).style.display !== 'none';
+  }
+
+  function el(id) {
+    return document.getElementById(id);
+  }
+
+  function collectMetrics(metrics, sessionTimeOffset, sessionStartTimestamp) {
+    return () => {
+      const metric = metrics.push();
+      const timeOffset = sessionTimeOffset + (Date.now() - sessionStartTimestamp);
+
+      let metricData = {
+        timestamp: Date.now(),
+        timeOffset: timeOffset,
+        clips: window.clips,
+        processors: window.processors,
+        memory: window.memory,
+        yomi: window.yomi,
+        operations: window.operations,
+        creativity: window.creativity
+      };
+
+      if (exists('businessDiv')) {
+        metricData = {...metricData,
+          funds: window.funds,
+          investments: window.portTotal,
+          autoclippers: window.clipmakerLevel,
+          megaclippers: window.megaClipperLevel,
+          trust: window.trust,
+        };
+      }
+
+      if (exists('powerDiv')) {
+        metricData = {...metricData,
+          availableMatter: window.availableMatter,
+          acquiredMatter: window.acquiredMatter,
+          factories: window.factoryLevel,
+          harvesters: window.harvesterLevel,
+          wireDrones: window.wireDroneLevel,
+        };
+      }
+
+      if (exists('probeDesignDiv')) {
+        metricData = {...metricData,
+          probeTrust: window.probeTrust,
+          probeCount: window.probeCount,
+          matterRemaining: window.totalMatter - window.foundMatter,
+          drifterCount: window.drifterCount,
+          honor: window.honor
+        };
+      }
+      
+      metric.set(metricData);
+
+      window.localStorage.setItem(TIME_OFFSET, timeOffset);
+    }
+  }
+})();
