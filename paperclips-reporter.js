c8fae97d234611a8dec8e29d2cf56fc557cd6901
Marc Litchfield <marclitchfield@gmail.com>
2017-12-01 07:35:59 -0800

    Report unusedClips (inventory) in phase 2

--- paperclips-reporter.js
+++ paperclips-reporter.js
@@ -111,6 +111,7 @@
           factories: window.factoryLevel,
           harvesters: window.harvesterLevel,
           wireDrones: window.wireDroneLevel,
+          inventory: window.unusedClips,
           performance: val('performance'),
           consumption: val('powerConsumptionRate'),
           production: val('powerProductionRate'),
